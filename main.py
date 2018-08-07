2f619cfba4dc73570ec39c61175a8a7d0635724b
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-08-08 04:01:18 +0900

    カロリーの問い合わせ処理を記述した

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ clova = Clova(application_id=application_id,
               default_language="ja", debug_mode=False)
 
 
+def getCalorieData(food_name):
+    import requests
+    r = requests.get(
+        'https://apex.oracle.com/pls/apex/evangelist/shokuhindb/food/' + food_name)
+    if len(r.json()['items']) != 0:
+        return r.json()['items'][0]['calorie']
+    else:
+        return ''
+
+
 @clova.handle.launch
 def launch_request_handler(clova_request):
     return clova.response("こんにちは世界。スキルを起動します")
@@ -16,11 +26,12 @@ def launch_request_handler(clova_request):
 
 @clova.handle.default
 def default_handler(clova_request):
-    if clova_request.slot_value('Foods') == 'たこ焼き':
-        return clova.response("たこ焼きのカロリーは32キロカロリーです")
-    if clova_request.slot_value('Foods') == 'お好み焼き':
-        return clova.response("お好み焼きのカロリーは518キロカロリーです")
-    return clova.response("もう一度お願いします")
+    try:
+        food_name = clova_request.slot_value('Foods')
+        calorie = getCalorieData(food_name)
+        return clova.response(str(food_name) + "のカロリーは" + str(calorie) + "キロカロリーです")
+    except:
+        return clova.response("すみません．もう一度お願いします")
 
 
 from flask import Flask, request, jsonify
